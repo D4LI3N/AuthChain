220f3ab4fc4b3df83f24b1e19113eff3a5e4f7cf
Daniel Petrovich <danielthe@cyberdude.com>
2024-05-09 09:16:25 +0200

    Update AuthChain_test.py

--- AuthChain_test.py
+++ AuthChain_test.py
@@ -1,34 +1,34 @@
 from AuthChain import AuthChain
 
-def registracija(username, password):
+def registration(username, password):
     blockchain.register_user('user1', 'password123')
-    return "    [*] Registrovan korisnik: " + username
+    return "    [*] User signed up: " + username
 
-def prijava(username, password):
-    return "Prijava uspešna!" if blockchain.login_user(username, password) else "Netočno korisničko ime ili lozinka!"
+def login(username, password):
+    return "Login successful!" if blockchain.login_user(username, password) else "Invalid username or password!"
 
-# Testiranje AuthChain-a
+# AuthChain test
 if __name__ == "__main__":
 
-    # Stvaranje nove instance mreže
+    # Create a new instance
     blockchain = AuthChain()
     
-    # Testni nalozi
-    testni_korisnici = [('user1', 'pass1',), ('user2', 'pass2',)]
+    # Test users
+    test_users = [('user1', 'pass1',), ('user2', 'pass2',)]
 
-    # Registracija testnih korisnika
-    for k_ime, k_loz in testni_korisnici:
-            print("[0]", "Registracija korisnika:",k_ime, k_loz)
-            print("[0]", registracija(k_ime, k_loz))
+    # Test user sign up
+    for k_name, k_pass in test_users:
+            print("[0]", "User registration:",k_name, k_pass)
+            print("[0]", registration(k_name, k_pass))
 
-    # 1. Pokušaj prijave sa točnim podatcima
-    print("[1]", "Pokušaj prijave sa točnim podatcima:", testni_korisnici[0][0], testni_korisnici[0][1])
-    print("[1]", prijava(testni_korisnici[0][0], testni_korisnici[0][1]) )
+    # 1. Correct data login test
+    print("[1]", "Correct data login test:", test_users[0][0], test_users[0][1])
+    print("[1]", login(test_users[0][0], test_users[0][1]) )
 
-    # 2. Pokušaj prijave sa netočnom lozinkom
-    print("[2]", "Pokušaj prijave sa netočnom lozinkom:", testni_korisnici[0][0], "netočna_lozinka")
-    print("[2]", prijava(testni_korisnici[0][0], "netočna_lozinka") )
+    # 2. Incorrect data login test
+    print("[2]", "Incorrect data login test:", test_users[0][0], "incorrect_password")
+    print("[2]", login(test_users[0][0], "incorrect_password") )
 
-    # 3. Pokušaj prijave sa ne postojećim korisnikom
-    print("[3]", "Pokušaj prijave sa netočnom lozinkom:", "nepostojeći", "netočna_lozinka")
-    print("[3]", prijava("nepostojeći", "netočna_lozinka") )
+    # 3. Unexistent user login
+    print("[3]", "Unexistent user login:", "unexistent", "incorrect_password")
+    print("[3]", login("unexistent", "incorrect_password") )
